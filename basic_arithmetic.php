0b3754711441f80fb96080c913b8acdc4b19f36a
NishidaShiko <ec2-user@ip-172-31-42-81.ap-northeast-1.compute.internal>
2024-03-20 06:05:25 +0000

    basic_arithmetic.phpを書いた

---
+++ basic_arithmetic.php
@@ -0,0 +1,34 @@
+<?php
+
+$i = random_int(1, 999);
+$j =random_int(1, 999);
+
+$k = $i + $j;
+
+echo "{$i} + {$j} = {$k} \n";
+
+$k = $i - $j;
+
+echo"{$i} - {$J} = {$k} \n";
+
+$k = $i * $j;
+
+echo "{$i} * {$j} = {$k} \n";
+
+
+$k = $i / $j;
+
+echo "{$i} / {$j} = {$k} \n";
+
+$k = $i % $j;
+
+echo "{$i} % {$j} = {$k} \n";
+
+$k =$i ** 3;
+
+echo "{$i} ** 3 = {$k} \n";
+
+$k = pow($i, 3);
+
+echo "pw ({$i}, 3) = {$k} \n";
+
